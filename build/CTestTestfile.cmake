9f5ca61f901ee0c8dac70f524278a34d88f25568
etiennedv <etiennedv3@gmail.com>
2024-05-01 15:31:53 +0000

    Removing Build, again

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jovyan/2270/graph-final-project
-# Build directory: /home/jovyan/2270/graph-final-project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
